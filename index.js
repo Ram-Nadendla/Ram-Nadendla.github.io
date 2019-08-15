cf3d3d615c876fcfb4c866818a8e200f56822f32
Ram_Nadendla <ramnadendla1111@gmail.com>
2019-08-15 22:39:14 +0530

    On Click of a tags , making the tab selected/deseled

---
+++ index.js
@@ -0,0 +1,33 @@
+function personal() {
+	
+	document.getElementById("p").className=document.getElementById("p").className.replace("active", " ");
+	document.getElementById("e").className=document.getElementById("e").className.replace("active", " ");
+	document.getElementById("t").className=document.getElementById("t").className.replace( "active" ," ");
+	document.getElementById("c").className=document.getElementById("c").className.replace("active"," ");
+	
+	document.getElementById("p").className=document.getElementById("p").className + " active";
+}
+function educational() {
+	document.getElementById("p").className=document.getElementById("p").className.replace("active", " ");
+	document.getElementById("e").className=document.getElementById("e").className.replace("active", " ");
+	document.getElementById("t").className=document.getElementById("t").className.replace( "active" ," ");
+	document.getElementById("c").className=document.getElementById("c").className.replace("active"," ");
+	
+	document.getElementById("e").className=document.getElementById("e").className + " active";
+}
+function technical() {
+	document.getElementById("p").className=document.getElementById("p").className.replace("active", " ");
+	document.getElementById("e").className=document.getElementById("e").className.replace("active", " ");
+	document.getElementById("t").className=document.getElementById("t").className.replace( "active" ," ");
+	document.getElementById("c").className=document.getElementById("c").className.replace("active"," ");
+	
+	document.getElementById("t").className=document.getElementById("t").className + " active";
+}
+function contact() {
+	document.getElementById("p").className=document.getElementById("p").className.replace("active", " ");
+	document.getElementById("e").className=document.getElementById("e").className.replace("active", " ");
+	document.getElementById("t").className=document.getElementById("t").className.replace( "active" ," ");
+	document.getElementById("c").className=document.getElementById("c").className.replace("active"," ");
+	
+	document.getElementById("c").className=document.getElementById("c").className + " active";
+}
